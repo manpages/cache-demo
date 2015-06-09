e15ecfba80837ce7cb1d06174c9b87706eb7a3ad
Jonn Mostovoy <amarr.industrial@gmail.com>
2015-06-09 22:27:39 +0200

    Implement strategy and threaded generators

--- cache.py
+++ cache.py
@@ -1,7 +1,8 @@
+from random import randint
 from pprint import pprint
-
-def initial_rows():
-  return {}
+import threading
+import time
+import sys
 
 def respond(req, rows0, size=5):
   rows = dict(rows0)
@@ -45,10 +46,70 @@ def drop_least_frequently_used_maybe(xs0, size):
 def fetch(x):
   return 2*x
 
+def pretty_print(req, size, requests):
+  global mailbox
+  global rows
+  pprint((req, mailbox, rows))
+
+###
+
+class cache_server(threading.Thread):
+  def __init__ (self, size, requests):
+    self.ticks    = 0
+    self.size     = size
+    self.requests = requests
+    threading.Thread.__init__ (self)
+  def run(self):
+    global mailbox
+    global rows
+    while True:
+      if len(mailbox) == 0:
+        if self.ticks > 110:
+          break
+        self.ticks = self.ticks + 1
+        time.sleep(0.01)
+      else:
+        self.ticks = 0
+        req = mailbox.pop(0)
+        rows = respond(req, rows, self.size)[1]
+        pretty_print(req, self.size, self.requests)
+
+class mailbox_server(threading.Thread):
+  def __init__ (self, requests):
+    global mailbox
+    for x in range(requests):
+      mailbox.append(randint(0, 9))
+    self.tick     = 0
+    self.requests = requests
+    threading.Thread.__init__ (self)
+  def run(self):
+    global mailbox
+    global rows
+    while True:
+      if self.tick > self.requests:
+        break
+      else:
+        self.tick = self.tick + 1
+        time.sleep(1)
+        mailbox.append(randint(0, 9))
+
+mailbox = []
+rows    = {}
+
 ###
 
 if __name__ == "__main__":
-  rows = initial_rows()
-  for x in [1,2,3,4,5,6,7,8,9,4,5,6,2,3,5,6,7,8]:
-    (line, rows) = respond(x, rows)
-    pprint(rows)
+  size = 5
+  requests = 5
+  pprint(sys.argv)
+  if len(sys.argv) > 1:
+    size = int(sys.argv[1])
+  if len(sys.argv) > 2:
+    requests = int(sys.argv[2])
+  for x in range(0, requests):
+    global mailbox
+    mailbox.append(randint(0, 9))
+  t = cache_server(size, requests)
+  t.start()
+  m = mailbox_server(requests)
+  m.start()
